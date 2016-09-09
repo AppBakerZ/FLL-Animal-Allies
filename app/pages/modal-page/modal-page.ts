c1b91d3857e6d5e230f04eb64ca8036f1be97cbe
Wasif KhanZada <wasifkhanzada309.wk@gmail.com>
2016-09-09 19:43:41 +0500

    saved list set and icon and splash screen set

--- app/pages/modal-page/modal-page.ts
+++ app/pages/modal-page/modal-page.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
-import {Platform, NavController, ModalController, ViewController} from 'ionic-angular';
+import {Platform, NavController, ModalController, ViewController, AlertController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/modal-page/modal-page.html'
 })
 export class SaveModal {
-  constructor(private navCtrl: NavController, public viewCtrl: ViewController) {
+  constructor(private navCtrl: NavController, public viewCtrl: ViewController, public alertCtrl: AlertController) {
+  }
+  showComment() {
+    let alert = this.alertCtrl.create({
+      title: 'Comment',
+      subTitle: ` Lorem Ipsum passages and more recently with desktop publishing
+      software like Aldus PageMaker including versions of Lorem Ipsum`,
+
+      buttons: ['OK']
+    });
+    alert.present();
   }
   dismiss() {
     this.viewCtrl.dismiss();
